d19d2ee20ebed26125ebdbcd5ea72b5cb0fdf1b1
Tang <sysu_tjrone@163.com>
2018-09-20 01:44:31 +0800

    add sync

--- concurrent/select/main.go
+++ concurrent/select/main.go
@@ -6,16 +6,19 @@ import (
 
 func main() {
 	c1, c2 := make(chan int), make(chan string)
+	o := make(chan bool, 2)
 	go func() {
 		for {
 			select {
 			case v, ok := <-c1:
 				if !ok {
+					o <- true
 					break
 				}
 				fmt.Println("c1传递了值进来", v)
 			case v, ok := <-c2:
 				if !ok {
+					o <- true
 					break
 				}
 				fmt.Println("c2", v)
@@ -28,4 +31,8 @@ func main() {
 	c2 <- "hello"
 	close(c1)
 	close(c2)
+
+	for i := 0; i < 2; i++ {
+		<-o
+	}
 }
